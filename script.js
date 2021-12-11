5ce9a146bdfb769b48706dfb3b7ccc9c407b4c0b
Kyro <mariogallardo1331@yahoo.com>
2021-12-11 13:28:57 -0600

    touching up JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// tells our document we are ready to load HTML & CSS. 
 $(document).ready(function () {
   var colorOn = function () {
     var actualTime = moment().hours();
@@ -6,9 +7,11 @@ $(document).ready(function () {
 
       if (blockTime < actualTime) {
         $(this).addClass("past");
+
       } else if (blockTime === actualTime) {
         $(this).removeClass("past");
         $(this).addClass("present");
+        
       } else {
         $(this).removeClass("past");
         $(this).removeClass("present");
@@ -17,25 +20,39 @@ $(document).ready(function () {
     });
   };
 
+  // calling our function
   colorOn();
 
+  // display current day & time using moment
   $("#currentDay").text(moment().format('LLLL'));
 
+  // saves info to local storage when save btn is clicked
   $(".saveBtn").on("click", function() {
       //set variables for key and value
+
       var key = $(this).parent().attr("id")
       var value = $(this).siblings(".description").val()
 
       //set local stroage
     localStorage.setItem(key, value);
-  }
-  )
+  });
 
-  $("#hour-9 .description").val(localStorage.getItem("hour-9"))
+  // load saved data from our local storage
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
 
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
 
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 });
-
-
-//localStorage.setItem("key", JSON.stringify(value))
-//JSON.parse(localStorage.getItem("key"))
